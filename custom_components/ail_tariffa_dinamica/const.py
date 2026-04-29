42132768049ee8ec8dcf67f78b79cca13f41b03a
Dav41K9 <d.quattrone@hotmail.com>
2026-04-29 21:15:34 +0200

    Update const.py

--- custom_components/ail_tariffa_dinamica/const.py
+++ custom_components/ail_tariffa_dinamica/const.py
@@ -1,35 +1,46 @@
 """Costanti per l'integrazione AIL Tariffa Dinamica."""
-DOMAIN = "ail_tariffa_dinamica"
-UPDATE_TIME = (18, 15)  # Ora di aggiornamento: 18:15
+from homeassistant.const import Platform
 
-# Mappatura fasce orarie
+DOMAIN = "ail_tariffa_dinamica"
+PLATFORMS = [Platform.SENSOR]
+
+# Configurazione dei sensori
+SENSOR_CONFIGS = {
+    "mattutina": {
+        "name": "Tariffa Mattutina AIL",
+        "icon": "mdi:weather-sunny",
+        "time_slot": "06:00 - 10:00"
+    },
+    "solare": {
+        "name": "Tariffa Solare AIL",
+        "icon": "mdi:weather-partly-cloudy",
+        "time_slot": "10:00 - 17:00"
+    },
+    "serale": {
+        "name": "Tariffa Serale AIL",
+        "icon": "mdi:weather-night-partly-cloudy",
+        "time_slot": "17:00 - 21:00"
+    },
+    "notturna": {
+        "name": "Tariffa Notturna AIL",
+        "icon": "mdi:weather-night",
+        "time_slot": "21:00 - 06:00"
+    }
+}
+
+# Mappatura nomi fascia → chiavi interne
 TIME_SLOTS = {
     "Mattutina": "mattutina",
-    "Solare": "solare", 
+    "Solare": "solare",
     "Serale": "serale",
     "Notturna": "notturna"
 }
 
-# Configurazione sensori
-SENSOR_CONFIGS = {
-    "mattutina": {
-        "name": "Tariffa Mattutina AIL",
-        "time_slot": "06:00-10:00",
-        "icon": "mdi:weather-sunny"
-    },
-    "solare": {
-        "name": "Tariffa Solare AIL", 
-        "time_slot": "10:00-17:00",
-        "icon": "mdi:solar-power"
-    },
-    "serale": {
-        "name": "Tariffa Serale AIL",
-        "time_slot": "17:00-22:00", 
-        "icon": "mdi:weather-night"
-    },
-    "notturna": {
-        "name": "Tariffa Notturna AIL",
-        "time_slot": "22:00-06:00",
-        "icon": "mdi:moon-waning-crescent"
-    }
+# Informazioni dispositivo per il Device Registry
+DEVICE_INFO = {
+    "manufacturer": "AIL (Aziende Industriali di Lugano)",
+    "model": "Tariffa Dinamica",
+    "name": "AIL Tariffa Dinamica",
+    "sw_version": "1.1.0",
+    "configuration_url": "https://www.ail.ch/aziende/elettricita/prodotti/Tariffa-dinamica/tariffa-dinamica.html"
 }
